5fd5597c3d06ee3e5ee2c92dd873d527b0cec032
Yokesh <yokeshspartan@gmail.com>
2021-07-29 15:03:29 +0530

    Day 2 commit - end

--- Day_Two.py
+++ Day_Two.py
@@ -10,3 +10,20 @@
 # print("Each person should pay", round(paid_amount, 1))
 
 
+# print(round(25.77))
+# -------------------------------------------
+# print(8//3) ans=2
+# -------------------------------------------
+# score = 0
+# height = 11.6
+# winning = True
+# print(f"Your score is {score},your height is {height},You are winning is {winning} ")
+# print("Your score is {0},your height is {1},You are winning is {2} ".format(score,height,winning))
+# -------------------------------------------
+#Days left
+
+age = int(input("Enter Age:"))
+days_left = (365*90) - (365*age)
+weeks_left = (52*90) - (52*age)
+months_left = (12*90) - (12*age)
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left")
